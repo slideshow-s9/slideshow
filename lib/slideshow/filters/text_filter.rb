edddceb723b178fec7a4ccc7eb4991c03ddb2ad0
Gerald Bauer <gerald.bauer@gmail.com>
2010-07-10 16:02:36 +0200

    add better code block patching

--- lib/slideshow/filters/text_filter.rb
+++ lib/slideshow/filters/text_filter.rb
@@ -34,9 +34,9 @@ def directives_percent_style( content )
     params    = $2
     if [ 'slide', 'style' ].include?( directive )  
       directive_single += 1
-      "<!-- _S9#{directive.upcase}_ #{params} -->"         
+      "<!-- _S9#{directive.upcase}_ #{params ? params : ''} -->"         
     else
-      "%#{directive} #{params ? params : ''}"  # skip block or unknown directives
+      Regexp.last_match( 0 ) # skip block or unknown directives
     end
   end
 
@@ -200,29 +200,39 @@ def comments_percent_style( content )
   def code_block_curly_style( content )
     # replace {{{  w/ <pre class='code'>
     # replace }}}  w/ </pre>
+    # use 4-6 { or } to escape back to literal value (e.g. {{{{ or {{{{{{ => {{{ )
+    # note: {{{ / }}} are anchored to beginning of line ( spaces and tabs before {{{/}}}allowed )
     
     # track statistics
-    code_begin = 0
-    code_end   = 0    
-    
-    content.gsub!( "{{{{{{", "<pre class='code'>_S9BEGIN_" )
-    content.gsub!( "}}}}}}", "_S9END_</pre>" )  
-    
-    content.gsub!( "{{{" ) do |match|
-      code_begin += 1
-      "<pre class='code'>"
+    code_begin     = 0
+    code_begin_esc = 0
+    code_end       = 0    
+    code_end_esc   = 0    
+        
+    content.gsub!( /^[ \t]*(\{{3,6})/ ) do |match|
+      escaped = ($1.length > 3)
+      if escaped
+        code_begin_esc += 1
+        "{{{"
+      else
+        code_begin += 1
+        "<pre class='code'>"
+      end
     end    
     
-    content.gsub!( "}}}" ) do |match|
-      code_end += 1
-      "</pre>"
+    content.gsub!( /^[ \t]*(\}{3,6})/ ) do |match|
+      escaped = ($1.length > 3)
+      if escaped
+        code_end_esc += 1
+        "}}}"
+      else
+        code_end += 1
+        "</pre>"      
+      end
     end
-    
-    # restore escaped {{{}}} 
-    content.gsub!( "_S9BEGIN_", "{{{" )
-    content.gsub!( "_S9END_", "}}}" )
-    
-    puts "  Patching code blocks (#{code_begin}/#{code_end} {{{/}}}-lines)..."    
+        
+    puts "  Patching {{{/}}}-code blocks (#{code_begin}/#{code_end} blocks, " +
+         "#{code_begin_esc}/#{code_end_esc} escaped blocks)..."    
     
     content
   end
