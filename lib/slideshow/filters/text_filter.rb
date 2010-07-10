8544049262b1c71c7c629aba5c1bffa4f53184c2
Gerald Bauer <gerald.bauer@gmail.com>
2010-07-11 00:16:52 +0200

    adding escape for django-style helpers

--- lib/slideshow/filters/text_filter.rb
+++ lib/slideshow/filters/text_filter.rb
@@ -179,7 +179,7 @@ def comments_percent_style( content )
     return params   if params.nil? || params.strip.empty?
 
     params.strip!    
-    ## todo: add ' and " for check??
+    ## todo: add check for " ??
     if params.include?( '=>' )
       puts "** warning: skipping patching of params for helper '#{method}'; already includes '=>':"
       puts "  #{params}"
@@ -190,30 +190,27 @@ def comments_percent_style( content )
     before = params.clone
     
     # 1) string-ify values and keys (that is, wrap in '')
-    params.gsub!( /[:a-zA-Z0-9][\w\/\-\.]*/) do |match|
-      symbol = ( Regexp.last_match( 0 ).include?(':') )
-      if symbol  # return symbols as is
-        Regexp.last_match( 0 )
+    #  plus separate w/ commas
+    params.gsub!( /([:a-zA-Z0-9][\w\/\-\.#]*)|('[^'\n]*')/) do |match|
+      symbol = ( Regexp.last_match( 0 )[0,1] == ':' )
+      quoted = ( Regexp.last_match( 0 )[0,1] == "'" )
+      if symbol || quoted  # return symbols or quoted string as is
+        "#{Regexp.last_match( 0 )},"
       else
-        "'#{Regexp.last_match( 0 )}'"
+        "'#{Regexp.last_match( 0 )}',"
       end
     end
-    
-    # 2) change = to =>
-    params.gsub!( /[ \t]*=[ \t]*/, '=>' )
-    
-    # 3) symbol-ize hash keys 
-    params.gsub!( /'(\w+)'=>/ ) do |match|
+        
+    # 2) symbol-ize hash keys
+    #    change = to =>
+    #    remove comma for key/value pairs
+    params.gsub!( /'(\w+)',[ \t]*=/ ) do |match|
       ":#{$1}=>"
     end
     
-    # 4) separate w/ commas
-    params.gsub!( /[ \t]*(:\w+)=>/) do |match|
-      ", #{$1}=>"
-    end
-    # remove possible leading comma
-    params.sub!( /^[ \t]*,/, '' )
-  
+    # 3) remove trailing comma
+    params.sub!( /[ \t]*,[ \t]*$/, '' ) 
+     
     puts "    Patching params for helper '#{method}' from '#{before}' to:"
     puts "      #{params}"  
        
@@ -234,30 +231,47 @@ def comments_percent_style( content )
     code
   end
 
-
   def erb_django_style( content )
 
     # replace expressions (support for single lines only)
     #  {{ expr }}  ->  <%= expr %>
     #  {% stmt %}  ->  <%  stmt %>   !! add in do if missing (for convenience)
+    #
+    # use use {{{ or {{{{ to escape expr back to literal value
+    # and use {%% %} to escape stmts
 
     erb_expr = 0
     erb_stmt_beg = 0
     erb_stmt_end = 0
 
-    content.gsub!( /\{\{([^{}\n]+?)\}\}/ ) do |match|
-      erb_expr += 1
-      "<%= #{erb_django_simple_params($1)} %>"
+    content.gsub!( /(\{{2,4})([^{}\n]+?)(\}{2,4})/ ) do |match|
+      escaped = ($1.length > 2)
+      if escaped
+        "{{#{$2}}}"
+      else
+        erb_expr += 1
+        "<%= #{erb_django_simple_params($2)} %>"        
+      end
     end
 
-    content.gsub!( /\{%[ \t]*end[ \t]*%\}/ ) do |match|
-      erb_stmt_end += 1
-      "<% end %>"
+    content.gsub!( /(\{%{1,2})([ \t]*end[ \t]*)%\}/ ) do |match|
+      escaped = ($1.length > 2)
+      if escaped
+        "{%#{$2}%}"
+      else
+        erb_stmt_end += 1
+        "<% end %>"
+      end
     end
 
-    content.gsub!( /\{%([^%\n]+?)%\}/ ) do |match|
-      erb_stmt_beg += 1
-      "<% #{erb_django_simple_params($1)} do %>"
+    content.gsub!( /(\{%{1,2})([^%\n]+?)%\}/ ) do |match|
+      escaped = ($1.length > 2)
+      if escaped
+        "{%#{$2}%}"
+      else
+        erb_stmt_beg += 1
+        "<% #{erb_django_simple_params($2)} do %>"
+      end
     end
 
     puts "  Patching embedded Ruby (erb) code Django-style (#{erb_expr} {{-expressions," +
